8107f785b12b78b00fcb447eb6d42d27da44c8b8
Abhiram Joshi <abramjoshi@gmail.com>
2021-03-26 01:41:06 +0530

    feat: Added MJML parser

--- mercury/mailer/serializers.py
+++ mercury/mailer/serializers.py
@@ -3,10 +3,11 @@ from rest_framework import serializers
 class EmailSerializer(serializers.Serializer):
     sender_name = serializers.CharField(max_length=50)
     sender_email = serializers.EmailField()
-    receipents = serializers.CharField(max_length=1000)
+    # recipients = serializers.ListField(child = serializers.EmailField(), allow_empty=False)
+    recipients = serializers.CharField(max_length=1000)
     subject = serializers.CharField(max_length=1000)
     body_text = serializers.CharField(max_length=5000)
-    body_html = serializers.CharField(max_length=5000)
+    body_mjml = serializers.CharField(max_length=5000)
     aws_region = serializers.CharField(max_length=20)
 
 
---
+++ mercury/mailer/utilities.py
@@ -0,0 +1,58 @@
+import boto3
+from botocore.exceptions import ClientError
+from mjml.mjml2html import mjml_to_html
+
+def send_email(**info):
+    SENDER = f"{info['sender_name']} <{info['sender_email']}>"
+
+    RECIPIENT = info['recipient']
+
+    AWS_REGION = info['aws_region']
+
+    SUBJECT = info['subject']
+
+    BODY_TEXT = info['body_text']
+
+    BODY_HTML = info['body_html']
+
+    CHARSET = "UTF-8"
+
+    client = boto3.client('ses',region_name=AWS_REGION)
+
+    try:
+
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                        },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+
+        )
+
+
+    except ClientError as e:
+        return e.response['Error']['Message']
+    else:
+        return f"Email sent! Message ID: {response['MessageId']}"
+
+def toHTML(mjml):
+    html = mjml_to_html(mjml)
+    return html['html']
