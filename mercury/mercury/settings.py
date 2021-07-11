2a9f1fc6014e895559642452fd22a8f6b45c1c2b
Abhiram-Joshi <abramjoshi@gmail.com>
2021-07-11 17:48:13 +0530

    fix: database connection

--- mercury/mercury/settings.py
+++ mercury/mercury/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from datetime import timedelta
 
+import dj_database_url
 import django_heroku
 from decouple import config
 from django.conf import settings
@@ -116,12 +117,8 @@ WSGI_APPLICATION = "mercury.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+DATABASES = {}
+DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
